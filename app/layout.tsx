79bb7795ff8060cbb880f89915a94fa2a896f76d
Prashant-S29 <prashant.s2922@gmail.com>
2024-02-28 21:53:26 +0530

    Feat: Metatags created and updated. SEO performance increased

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,25 @@ const montserrat = Montserrat({ subsets: ["cyrillic"] });
 export const metadata: Metadata = {
   title: "ICEPADM | SKIT",
   description: "Website for ICEPADM conferences at SKIT Jaipur",
+  openGraph: {
+    type: "website",
+    url: "https://icepadm.skit.ac.in/",
+    title: "ICEPADM | SKIT",
+    description: "Website for ICEPADM conferences at SKIT Jaipur",
+    siteName: "Designed and Developed by Prashant Singh | @httpsPrashant on Twitter",
+    images: [
+      {
+        url: "https://res.cloudinary.com/denl2iizs/image/upload/v1709137066/kijhxczp42nqbmuik8ih.png",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "ICEPADM | SKIT",
+    creator: "Designed and Developed by Prashant Singh | @httpsPrashant on Twitter",
+    images:
+      "https://res.cloudinary.com/denl2iizs/image/upload/v1709137066/kijhxczp42nqbmuik8ih.png",
+  },
 };
 
 export default function RootLayout({
